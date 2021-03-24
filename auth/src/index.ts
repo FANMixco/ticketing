dd6e21237e44e4e42f073944f10800dc4d9db32f
¨Federico <fanmixco@gmail.com>
2021-03-24 15:43:43 +0100

    Configured kubernetes
    Configured basic kubernetes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,8 +1,13 @@
-import express from 'express';
-
+const express = require('express');
 const app = express();
+
 app.use(express.json());
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000');
+app.get('/api/users/currentuser', (req:any, res:any) => {
+    res.send('hey there!');
 });
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000!');
+});
+
